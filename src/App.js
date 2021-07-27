8692f46c41f18d6fa4d3e19102f5396beb8acff4
satinder36 <satinder.singh@vithysys.com>
2021-07-28 00:37:08 +0530

    user login added

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ import Users from "./components/Users";
 import { useState } from "react";
 import { Button } from "@material-ui/core";
 function App() {
-  //const [user, setUser] = useState("");
-  // const handleLogin = () => {
-  //   if (!user) {
-  //     auth.signInWithPopup(provider).then((result) => {
-  //       setUser(result.user);
-  //     });
-  //   }
-  // };
-  const [user, setUser] = useState({
-    displayName: "Alexa",
-    email: "ssatinder1996@gmail.com",
-    emailVerified: true,
-    phoneNumber: null,
-    photoURL:
-      "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg",
-  });
+  const [user, setUser] = useState("");
+  const handleLogin = () => {
+    if (!user) {
+      auth.signInWithPopup(provider).then((result) => {
+        setUser(result.user);
+      });
+    }
+  };
+  // const [user, setUser] = useState({
+  //   displayName: "Alexa",
+  //   email: "ssatinder1996@gmail.com",
+  //   emailVerified: true,
+  //   phoneNumber: null,
+  //   photoURL:
+  //     "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg",
+  // });
   console.log(user.email, "useremail");
   return (
     <Router>
